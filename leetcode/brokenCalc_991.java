83f0adea0439ea73a2c7c21da0b5c3df2c090b1e
Kaiyan Zheng <kz2324@columbia.edu>
2019-12-11 21:30:05 -0500

    update

---
+++ leetcode/brokenCalc_991.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class brokenCalc_991 {
+
+    public int brokenCalc(int X, int Y) {
+        int count = 0;
+        while (Y > X) {
+            Y = Y % 2 > 0 ? Y + 1 : Y / 2;
+            count++;
+        }
+        return count + X - Y;
+    }
+
+}
